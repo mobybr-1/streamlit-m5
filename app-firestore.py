248d7304b7486c74546d2227e135818426eba46a
mobybr-1 <125281224+mobybr-1@users.noreply.github.com>
2023-03-06 15:05:55 +0000

    deploy prod

--- app-firestore.py
+++ app-firestore.py
@@ -1,6 +1,18 @@
 import streamlit as st
-import pandas as pd 
+import pandas as pd
 from google.cloud import firestore
+from google.oauth2 import service_account
+
+import json
+key_dict = json.loads(st.secrets["textkey"])
+creds = service_account.Credentials.from_service_account_info(key_dict)
+db = firestore.Client(credentials=creds, project="names-project-demo")
+
+#import json
+#key_dict = json.loads(st.secrets["textkey"])
+#creds = service_account.Credentials.from_service_account_info(key_dict)
+#db = firestore.Client(credentials=creds, project="names")
+
 
 db=firestore.Client.from_service_account_json('bruno-demo2-firebase-adminsdk-gsfo7-5d00cefefe.json')
 dbNames=db.collection('names')
